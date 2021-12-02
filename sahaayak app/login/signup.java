08f47d6cf524a85750a487392a53ee04a2f8db26
NisargBarot22 <90953310+NisargBarot22@users.noreply.github.com>
2021-12-02 12:39:33 +0530

    Update signup.java

--- sahaayak app/login/signup.java
+++ sahaayak app/login/signup.java
@@ -1,1 +1,52 @@
 
+package com.technic.loginui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private EditText edUsername;
+    private EditText edPassword;
+    private EditText edConfirmPassword ;
+    private Button btnCreateUser;
+
+    private final String CREDENTIAL_SHARED_PREF = "our_shared_pref";
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        edUsername = findViewById(R.id.ed_username);
+        edPassword = findViewById(R.id.ed_password);
+        edConfirmPassword = findViewById(R.id.ed_confirm_pwd);
+        btnCreateUser = findViewById(R.id.btn_create_user);
+
+        btnCreateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strPassword = edPassword.getText().toString();
+                String strConfirmPassword = edConfirmPassword.getText().toString();
+                String strUsername = edUsername.getText().toString();
+
+                if(strPassword!=null && strConfirmPassword!=null && strPassword.equalsIgnoreCase(strConfirmPassword)){
+                    SharedPreferences credentials = getSharedPreferences(CREDENTIAL_SHARED_PREF, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = credentials.edit();
+                    editor.putString( "Password", strPassword);
+                    editor.putString( "Username", strUsername);
+                    editor.commit();
+
+                    SignupActivity.this.finish();
+                }
+            }
+    });
+}}
