e83b036131586dc8aa723d2c2a0f5a899f695daa
hariharaniyer12 <91085597+hariharaniyer12@users.noreply.github.com>
2021-12-03 15:28:43 +0530

    Update healthtracker.java

--- sahaayak app/healthtracker/healthtracker.java
+++ sahaayak app/healthtracker/healthtracker.java
@@ -1,1 +1,29 @@
+package com.example.healthtracker;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        }
+    public void button1(View w){
+        Intent intent=new Intent(getApplicationContext(), MainActivity2.class);
+        startActivity(intent);
+    }
+    public void button2(View w){
+        Intent intent=new Intent(getApplicationContext(), MainActivity4.class);
+        startActivity(intent);
+    }
+    public void button3(View v){
+        Intent intent=new Intent(getApplicationContext(), MainActivity3.class);
+        startActivity(intent);
+    }
+
+}
