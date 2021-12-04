15d3629ee64354ec2bcde21d6efdd164cd0c9095
hariharaniyer12 <91085597+hariharaniyer12@users.noreply.github.com>
2021-12-05 01:40:10 +0530

    Update healthtracker.java

--- sahaayak app/healthtracker/healthtracker.java
+++ sahaayak app/healthtracker/healthtracker.java
@@ -2,28 +2,53 @@ package com.example.healthtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity4 extends AppCompatActivity {
+    EditText weight,height,age;
+    TextView result;
+    RadioGroup radioButton;
+    RadioButton male,female;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        }
-    public void button1(View w){
-        Intent intent=new Intent(getApplicationContext(), MainActivity2.class);
-        startActivity(intent);
-    }
-    public void button2(View w){
-        Intent intent=new Intent(getApplicationContext(), MainActivity4.class);
-        startActivity(intent);
-    }
-    public void button3(View v){
-        Intent intent=new Intent(getApplicationContext(), MainActivity3.class);
-        startActivity(intent);
-    }
+        setContentView(R.layout.activity_main4);
+        result= findViewById(R.id.Answer);
+        weight=findViewById(R.id.Weight);
+        height=findViewById(R.id.Height);
+        age= findViewById(R.id.Age);
+        male= findViewById(R.id.rb1);
+        female= findViewById(R.id.rb2);
+        button= findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double w,h,r,a;
+                w=Double.parseDouble(weight.getText().toString());
+                h=Double.parseDouble(height.getText().toString());
+                a=Double.parseDouble(age.getText().toString());
 
-}
+                    switch (radioButton.getCheckedRadioButtonId()) {
+                        case R.id.rb1:
+                            r = 10 * w + 6.25 * h - 5 * a + 5;
+                            result.setText(String.valueOf(r));
+                            break;
+                        case R.id.rb2:
+                            r = 10 * w + 6.25 * h - 5 * a - 161;
+                            result.setText(String.valueOf(r));
+                            break;
+
+                    }
+
+
+            }
+            });
+        }
+    }
