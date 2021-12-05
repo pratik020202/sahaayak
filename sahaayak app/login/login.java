4e7b145bb1a4e6e7a5797b8599da57610147b1f2
NisargBarot22 <90953310+NisargBarot22@users.noreply.github.com>
2021-12-05 21:16:51 +0530

    Update login.java

--- sahaayak app/login/login.java
+++ sahaayak app/login/login.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,10 +15,12 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText edUsername;
-    private EditText edPassword;
+    private EditText username;
+    private EditText password;
     private Button btnLogin ;
     private Button btnSignUP ;
+
+    DBHelper myDB;
 
     private final String CREDENTIAL_SHARED_PREF = "our_shared_pref";
 
@@ -26,35 +29,34 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        edUsername = findViewById(R.id.ed_username);
-        edPassword = findViewById(R.id.ed_password);
+        username = findViewById(R.id.usernameLogin);
+        password = findViewById(R.id.passwordLogin);
         btnLogin = findViewById(R.id.button5);
         btnSignUP = findViewById(R.id.button6);
 
+        myDB = new DBHelper( this);
        // final Context context =this;
 
         btnSignUP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-               // Toast.makeText( LoginActivity.this,"Click on signup", Toast.LENGTH_SHORT).show();
-              // Intent i = new Intent(LoginActivity.this, SignupActivity.class);
                 Intent i = new Intent(LoginActivity.this , SignupActivity.class);
-                 startActivity(i);
+                startActivity(i);
                 Toast.makeText( LoginActivity.this," you Clicked on signup", Toast.LENGTH_SHORT).show();
 
 
             }
         });
 
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences credentials = getSharedPreferences(CREDENTIAL_SHARED_PREF, Context.MODE_PRIVATE);
-                String username_from_ed = edUsername.getText().toString();
-                String password_from_ed = edPassword.getText().toString();
-                String strUsername = "Nisarg";
-                String strPassword = "P1234";
+             //   SharedPreferences credentials = getSharedPreferences(CREDENTIAL_SHARED_PREF, Context.MODE_PRIVATE);
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+            //    String strUsername = "Nisarg";
+              //  String strPassword = "P1234";
             //    Toast.makeText( LoginActivity.this,"Login Successful", Toast.LENGTH_SHORT).show();
 
                 //Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
@@ -62,17 +64,32 @@ public class LoginActivity extends AppCompatActivity {
 
                // String username_from_ed = edUsername.getText().toString();
                 //String password_from_ed = edPassword.getText().toString();
-                if (username_from_ed.isEmpty() )
-                {
-                    Toast.makeText( LoginActivity.this,"Username cannot be blank", Toast.LENGTH_SHORT).show();
 
-                return;
+                if (user.isEmpty() || pass.isEmpty() )
+                {
+                    Toast.makeText( LoginActivity.this,"Username and password cannot be blank", Toast.LENGTH_SHORT).show();
+
+             //   return;
                 }
-                if(password_from_ed.isEmpty()){
+                else
+                {
+                    Boolean result = myDB.checkusernamePassword(user,pass);
+                    if(result==true){
+ // "YAHA PE NEXT PAGE KA NAME.CLASS DAALNA HAI"
+                  //      Intent intent = new Intent (getApplicationContext(), welcome.class);
+                   //     startActivity(intent);
+                        Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+                    }
+                }
+               /* if(password_from_ed.isEmpty()){
                     Toast.makeText( LoginActivity.this,"Password cannot be blank", Toast.LENGTH_SHORT).show();
                 return;
-                }
-                if(strUsername !=null && username_from_ed !=null && strUsername.equalsIgnoreCase(username_from_ed)){
+                }*/
+             /*   if(strUsername !=null && username_from_ed !=null && strUsername.equalsIgnoreCase(username_from_ed)){
                     if(strPassword!= null && password_from_ed !=null && strPassword.equalsIgnoreCase(password_from_ed)) {
                         Toast.makeText( LoginActivity.this,"Login Successful", Toast.LENGTH_SHORT).show();
                     } else {
@@ -82,9 +99,11 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.makeText(  LoginActivity.this,"Login Failed", Toast.LENGTH_SHORT).show();
 
 
-                    }
+                    }*/
+
 
                 }});
     };
 }
+
 
