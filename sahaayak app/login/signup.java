6ea1b1dcfb3595249610070b38e1c4392ff25648
NisargBarot22 <90953310+NisargBarot22@users.noreply.github.com>
2021-12-05 21:18:50 +0530

    Update signup.java

--- sahaayak app/login/signup.java
+++ sahaayak app/login/signup.java
@@ -10,13 +10,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private EditText edUsername;
-    private EditText edPassword;
-    private EditText edConfirmPassword ;
+    private EditText username;
+    private EditText password;
+    private EditText confirm_pwd ;
     private Button btnCreateUser;
+    DBHelper myDB;
 
     private final String CREDENTIAL_SHARED_PREF = "our_shared_pref";
 
@@ -26,19 +28,21 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        edUsername = findViewById(R.id.ed_username);
-        edPassword = findViewById(R.id.ed_password);
-        edConfirmPassword = findViewById(R.id.ed_confirm_pwd);
+        username = (EditText) findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        confirm_pwd = findViewById(R.id.confirm_pwd);
         btnCreateUser = findViewById(R.id.btn_create_user);
+
+        myDB = new DBHelper( this);
 
         btnCreateUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String strPassword = edPassword.getText().toString();
-                String strConfirmPassword = edConfirmPassword.getText().toString();
-                String strUsername = edUsername.getText().toString();
+                String pass = password.getText().toString();
+                String repass = confirm_pwd.getText().toString();
+                String user = username.getText().toString();
 
-                if(strPassword!=null && strConfirmPassword!=null && strPassword.equalsIgnoreCase(strConfirmPassword)){
+            /*    if(strPassword!=null && strConfirmPassword!=null && strPassword.equalsIgnoreCase(strConfirmPassword)){
                     SharedPreferences credentials = getSharedPreferences(CREDENTIAL_SHARED_PREF, Context.MODE_PRIVATE);
                     SharedPreferences.Editor editor = credentials.edit();
                     editor.putString( "Password", strPassword);
@@ -46,7 +50,34 @@ public class SignupActivity extends AppCompatActivity {
                     editor.commit();
 
                     SignupActivity.this.finish();
+                }*/
+                if (user.equals("") || pass.equals("") || repass.equals(""))
+                {
+                    Toast.makeText(SignupActivity.this, "Fill all the fields", Toast.LENGTH_SHORT).show();
                 }
-            }
-    });
+
+
+                else{
+                    if(pass.equals(repass)) {
+                    //   DBHelper myDB = null;
+                        Boolean usercheckResult = myDB.checkusername(user);
+                        if (usercheckResult == false) {
+                            Boolean regResult = myDB.insertData(user, pass);
+                            if (regResult == true) {
+                                Toast.makeText(SignupActivity.this, "Register Successful", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(SignupActivity.this, "Register Unsuccessful", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(SignupActivity.this, "User already Exists.\n Please Sign In", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                        else
+                        {
+                            Toast.makeText(SignupActivity.this, "Password not matching",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            });
+
 }}
