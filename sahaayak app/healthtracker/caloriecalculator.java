48b271b89d1846cca6e9b0d1dedcbabebda02641
pratik020202 <91137425+pratik020202@users.noreply.github.com>
2021-12-16 13:41:46 +0530

    Update caloriecalculator.java

--- sahaayak app/healthtracker/caloriecalculator.java
+++ sahaayak app/healthtracker/caloriecalculator.java
@@ -2,45 +2,43 @@ package com.example.healthtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class MainActivity4 extends AppCompatActivity {
+public class CalorieCalculator extends AppCompatActivity {
+
     EditText weight,height,age;
-    RadioGroup radioGroup;
-    Button button;
+    Button calculate;
     TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main6);
-        weight=findViewById(R.id.we);
-        height=findViewById(R.id.he);
-        age=findViewById(R.id.ag);
-        result=findViewById(R.id.finalanswer);
-        radioGroup= findViewById(R.id.radioGroup2);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        setContentView(R.layout.activity_calorie_calculator);
+        weight = findViewById(R.id.we);
+        height = findViewById(R.id.he);
+        age = findViewById(R.id.ag);
+        result = findViewById(R.id.finalanswer);
+        calculate = findViewById(R.id.calculate);
+        calculate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                double Weight,Age,Height,Result;
-                Weight=Double.parseDouble(weight.getText().toString());
-                Age=Double.parseDouble(age.getText().toString());
-                Height=Double.parseDouble(age.getText().toString());
-                switch (checkedId){
-                    case R.id.male:{
-                        Result= (Weight*100)*(Height*6.25)*(Age*5)/5 ;
-                        result.setText(String.valueOf(Result));
-                    }
-                    case R.id.female:{
-                        Result= (Weight*100)/(Height*6.25)/(Age*5)*161 ;
-                        result.setText(String.valueOf(Result));
-                    }
-                }
+            public void onClick(View view) {
+                openNewActivity();
             }
         });
+
+
+    }
+    public void openNewActivity(){
+        double Weight, Age, Height, ResultM, ResultF;
+        Weight = Double.parseDouble(weight.getText().toString());
+        Age = Double.parseDouble(age.getText().toString());
+        Height = Double.parseDouble(age.getText().toString());
+        ResultM = (Weight * 100) * (Height * 6.25) * (Age * 5) / 5;
+        result.setText(String.valueOf(ResultM));
 
     }
 }
